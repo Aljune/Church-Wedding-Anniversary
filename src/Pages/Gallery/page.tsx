3967b30ca4305dd1a123a9ad4189110e0869a248
degamAljune <degamoaljune14@gmail.com>
2025-07-09 20:23:12 +0800

    My Love Forever 22

--- src/Pages/Gallery/page.tsx
+++ src/Pages/Gallery/page.tsx
@@ -1,25 +1,10 @@
-import React, { useState, useEffect } from 'react';
-
-import GalleryComponent from '../../components/container/Gallery/GalleryContainer';
-import { useLocation } from 'react-router-dom';
+import React, { FC } from 'react';
 
 const GalleryPage = () => {
 
-    const [user, setUser] = useState<string>('user');
-
-    const location = useLocation();
-    const { pathname } = location;
-    const checkPathName = () => {
-        if(pathname === '/admin/user-gallery' ){
-            setUser('admin');
-        }
-    }
-
-    useEffect(checkPathName,[]);
     return (
         <>
-         Current Path: {user}
-            <GalleryComponent role={user}/>
+         Current Path
         </>
     );
   };
